2f587f535d6e378c091f86cd392c74c2a289ebdd
Pankaj Dewangan <dewanganpankaj246@gmail.com>
2026-06-23 11:42:25 +0530

    Implement the user details service for the security config In In-memory manner

--- Security/src/main/java/com/example/Security/config/WebSecurityConfig.java
+++ Security/src/main/java/com/example/Security/config/WebSecurityConfig.java
@@ -1,6 +1,5 @@
 package com.example.Security.config;
 
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -8,6 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -16,47 +16,39 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig {
+
     @Bean
-    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception
-    {
-        //Bean is required while you try to implement the
-        //implement the UserDetailsServiceRepo
-        httpSecurity.authorizeHttpRequests(auth ->auth
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+
+        httpSecurity
+                .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/posts").permitAll()
-                .anyRequest().authenticated())
+                        .anyRequest().authenticated())
                 .formLogin(Customizer.withDefaults());
 
         return httpSecurity.build();
     }
 
-    // Another bean is also created for the user details services
     @Bean
-    UserDetailsService myInMemoryUserDetailsServices()
-    {
-        // the User is present inside the spring security
-        // single user
-        UserDetails userDetails = User
+    public UserDetailsService myInMemoryUserDetailsServices() {
+
+        UserDetails user = User
                 .withUsername("Panku")
                 .password(passwordEncoder().encode("Pass"))
-                .roles("User")
+                .roles("USER")
                 .build();
 
-        // same like multipple user
-        // single user
-        UserDetails Admin_user = User
-                .withUsername("Panku")
-                .password(passwordEncoder().encode("Pass"))
-                .roles("User")
+        UserDetails admin = User
+                .withUsername("Admin")
+                .password(passwordEncoder().encode("Admin123"))
+                .roles("ADMIN")
                 .build();
+
+        return new InMemoryUserDetailsManager(user, admin);
     }
 
-
     @Bean
-    PasswordEncoder passwordEncoder()
-    {
+    PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-
-
-
 }
